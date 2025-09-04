4e22dd5767ad474b5c1d9f406cb3190e487455cb
Finn <FB.Panhuijsen@student.han.nl>
2025-09-04 15:14:48 +0200

    Added iteration depth parameter & added base case

--- src/main.hs
+++ src/main.hs
@@ -1,27 +1,25 @@
 -- Fractal Generator made by Kyotem
--- Last Edit: 03/09/2025 =
+-- Last Edit: 04/09/2025 =
 
 -- TODO: Create separate functions for calculating Julia & Mandlebrot sets
--- Can this be condensed?
--- equation :: Float -> Float
--- -- c = 0.2 
--- equation n = n*n + 0.2 
---             equation n
 
--- Look into executing Recursion properly (Base condition, etc)
--- equation n = equation(n*n + 0.2) 
-
--- test :: Float
 -- test (n :: Float, d :: Int) = n*n + 0.2
 
-test :: Float -> Float
-test n =
-    if n > 2 then 3
-    else test(n*n + 0.2)
 
+-- Signature works like: Param1 -> Param2 -> Return type
+test :: Float -> Int -> Float
+
+-- TODO: Convert to using guards for this: https://www.futurelearn.com/info/courses/functional-programming-haskell/0/steps/27226
+-- Is much more readable & much better haskell-wise
+test n d =
+    -- Base case
+    if d < 0 then n
+    -- Prevent infinity
+    else if n > 2 then 3
+    else test (n*n + 0.2) (d - 1)
 
 
 main :: IO ()
 main = do
-    print(test 1.2)
+    print(test 0.3 5)
     
