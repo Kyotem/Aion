fd74ab58802fb057d20564230e653e4d012732db
Finn <FB.Panhuijsen@student.han.nl>
2025-09-04 17:39:54 +0200

    Updated to include guards

--- src/main.hs
+++ src/main.hs
@@ -1,25 +1,20 @@
 -- Fractal Generator made by Kyotem
--- Last Edit: 04/09/2025 =
-
--- TODO: Create separate functions for calculating Julia & Mandlebrot sets
-
--- test (n :: Float, d :: Int) = n*n + 0.2
-
+-- Last Edit: 04/09/2025
 
 -- Signature works like: Param1 -> Param2 -> Return type
-test :: Float -> Int -> Float
+test :: Float -> Float -> Int -> Float
 
--- TODO: Convert to using guards for this: https://www.futurelearn.com/info/courses/functional-programming-haskell/0/steps/27226
--- Is much more readable & much better haskell-wise
-test n d =
-    -- Base case
-    if d < 0 then n
-    -- Prevent infinity
-    else if n > 2 then 3
-    else test (n*n + 0.2) (d - 1)
+-- TODO: calculate equation manually and check if function is being calculated properly (Seems to return 3 in some cases where it LIKELY shouldn't but unsure)
+-- Based on: https://www.futurelearn.com/info/courses/functional-programming-haskell/0/steps/27226 (Reference)
+-- ! TODO: Can this singular function be used for both Julia & Mandlebrot sets? (Figure out)
+-- n = z_0 | c = constant | d = iteration depth
+test n c d -- C1: Recursion
+    | d < 0 = n -- Base Case
+    | n > 2 = 3 -- If escapes into infinity, return 3 (TODO: Create better handling for this) 
+    | otherwise = test (n*n + c) c (d-1) -- Recursive iteration to check whether or not n(z_n) escapes into infinity
 
+-- Recommended depth is around d=500 to check for infinity-escape (.. Source? Read it somewhere... TODO: Cite source)
 
 main :: IO ()
 main = do
-    print(test 0.3 5)
-    
+    print(test 1 4 500)
