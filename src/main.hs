97a2f9671d4390b304c61d4be1efff03f3b6ca25
Finn <FB.Panhuijsen@student.han.nl>
2025-09-04 15:04:41 +0200

    Implemented basic recursive call
    Does not have a base condition, currently only a check for checking if it escapes  into infinity

--- src/main.hs
+++ src/main.hs
@@ -3,14 +3,25 @@
 
 -- TODO: Create separate functions for calculating Julia & Mandlebrot sets
 -- Can this be condensed?
-equation :: Float -> Float
--- c = 0.2 
-equation n = n*n + 0.2
+-- equation :: Float -> Float
+-- -- c = 0.2 
+-- equation n = n*n + 0.2 
+--             equation n
 
 -- Look into executing Recursion properly (Base condition, etc)
 -- equation n = equation(n*n + 0.2) 
 
+-- test :: Float
+-- test (n :: Float, d :: Int) = n*n + 0.2
+
+test :: Float -> Float
+test n =
+    if n > 2 then 3
+    else test(n*n + 0.2)
+
+
+
 main :: IO ()
 main = do
-    print(equation 1.9)
+    print(test 1.2)
     
