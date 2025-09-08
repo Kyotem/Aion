adf1fac6e73b5d45f995335d36210817c51a8daf
Finn <150026629+Kyotem@users.noreply.github.com>
2025-09-08 18:45:45 +0200

    Improved separate modules & code clarity

--- src/Fractals.hs
+++ src/Fractals.hs
@@ -1,8 +1,19 @@
--- Fractal Generator (Renderer Module) made by Kyotem
--- Last Edit: 2025-09-08
-module Fractals (hasEscaped, calcDelta, genC, generateGrid, generateJulia) where
-import Data.Complex (Complex((:+)), magnitude)
+{-|
+Module      : Fractal Calculator
+Description : Used to calculate Julia & Mandelbrot sets
+Copyright   : (c) Finn Panhuijsen, 2025
+License     : BSD-3-Clause
+Maintainer  : FB.Panhuijsen@student.han.nl
+Stability   : experimental
 
+Last Edited: 2025-09-08
+Module has to be adjusted for:
+- Optimization
+- Less duplicate code
+
+-}
+module Fractals (hasEscaped, calcDelta, genComplex, generateMandelbrot, generateJulia) where
+import Data.Complex (Complex((:+)), magnitude)
 
 calcDelta :: Int -> Double -> Double -> Double
 calcDelta u n_min n_max = (n_max - n_min) / fromIntegral (u - 1)
@@ -17,21 +28,19 @@ hasEscaped c z maxIter
     | otherwise = hasEscaped c (z*z + c) (maxIter - 1)  -- TODO: I feel like this isn't calculating per the equations i built before, please cross-ref!!!
 
 -- ! Is there any use in x_min/max etc here if I'm going to scan over a grid regardless? (The generated grid would define steps, etc)
-genC :: Int -> Int -> Double -> Double -> Double -> Double -> Int -> Int -> Complex Double
-genC w h x_min x_max y_min y_max px py =
+genComplex :: Int -> Int -> Double -> Double -> Double -> Double -> Int -> Int -> Complex Double
+genComplex w h x_min x_max y_min y_max px py =
     (x_min + fromIntegral px * calcDelta w x_min x_max)
     :+ (y_max - fromIntegral py * calcDelta h y_min y_max)
 -- Using fromIntegral for conversion: https://wiki.haskell.org/Converting_numbers
 -- Resolve issues when inputting the parameters (Negative numbers can be seen as numeric litearls whilst it is expecting a double)
 -- TODO: Improve type signature & calculations (Somewhere later)
 
-
-
-
 -- Generate the grid of escape values
-generateGrid :: Int -> Int -> Double -> Double -> Double -> Double -> Int -> (Complex Double -> Complex Double -> Int -> Int) -> [[Int]]
-generateGrid w h x_min x_max y_min y_max maxIter f = 
-    [ [ f (genC w h x_min x_max y_min y_max px py) 0  maxIter | px <- [0..w-1] ] | py <- [0..h-1] ]
+-- ? Should this be moved to Renderer.hs?
+generateMandelbrot :: Int -> Int -> Double -> Double -> Double -> Double -> Int -> (Complex Double -> Complex Double -> Int -> Int) -> [[Int]]
+generateMandelbrot w h x_min x_max y_min y_max maxIter f = 
+    [ [ f (genComplex w h x_min x_max y_min y_max px py) 0  maxIter | px <- [0..w-1] ] | py <- [0..h-1] ]
     
 
 cJulia :: Complex Double
@@ -40,11 +49,9 @@ cJulia = (-0.7) :+ 0.27015
 -- FIXME: Function to generate Julia set grid (Duplicate code, please refactor!!)
 generateJulia :: Int -> Int -> Double -> Double -> Double -> Double -> Int -> (Complex Double -> Complex Double -> Int -> Int) -> [[Int]]
 generateJulia w h x_min x_max y_min y_max maxIter f =
-    [ [ f cJulia (genC w h x_min x_max y_min y_max px py) maxIter
-        | px <- [0..w-1] ]
-      | py <- [0..h-1] ]
+    [ [ f cJulia (genComplex w h x_min x_max y_min y_max px py) maxIter | px <- [0..w-1] ] | py <- [0..h-1] ]
 {-
-    f == Mandlebrot.hasEscaped (Can later be converted to map to a specific range)
+    f == Fractals.hasEscaped (Can later be converted to map to a specific range)
     - https://wiki.haskell.org/List_comprehension
     - https://www.youtube.com/watch?v=dYKQSWtJv-w
 
--- src/main.hs
+++ src/main.hs
@@ -2,30 +2,21 @@
 -- Last Edit: 2025-09-08
 module Main where
 
-import Fractals(generateJulia, hasEscaped)
+import Fractals(generateMandelbrot, hasEscaped)
 import Renderer
 
-{- 
-!!! Haskell has native support for complex numbers !!!
-https://hackage.haskell.org/package/base-4.21.0.0/docs/Data-Complex.html
-Can make your calculations MUCH more readable without needing to convert to plain arithm.
--}
--- Required to import functions next to the namespace import / (Module)
--- import Data.Complex (Complex((:+)), realPart, imagPart, magnitude)
-
-
-
+-- TODO: Add user input to decide what to do
 main :: IO ()
 main = do
     let w = 1000    -- Width of the grid (number of pixels)
         h = 1000    -- Height of the grid (number of pixels)
-        x_min = -1.5
+        x_min = -1.5 -- x to map the grid to
         x_max =  1.5
-        y_min = -1.5
+        y_min = -1.5 -- y to map the grid to
         y_max =  1.5    
         maxIter = 1000 -- Maximum number of iterations
 
     -- Generate the Mandelbrot set grid
-    let grid = generateJulia w h x_min x_max y_min y_max maxIter hasEscaped
+    let grid = generateMandelbrot w h x_min x_max y_min y_max maxIter hasEscaped
 
     printMatrix grid
