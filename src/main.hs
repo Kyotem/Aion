c24e49c15269e6acd7d2a5a8441a0267a869666e
Finn <150026629+Kyotem@users.noreply.github.com>
2025-09-08 12:42:50 +0200

    Adjusted the way we check for z > 2

--- src/main.hs
+++ src/main.hs
@@ -8,19 +8,19 @@ https://hackage.haskell.org/package/base-4.21.0.0/docs/Data-Complex.html
 Can make your calculations MUCH more readable without needing to convert to plain arithm.
 -}
 -- Required to import functions next to the namespace import / (Module)
-import Data.Complex (Complex((:+)), realPart, imagPart)
+import Data.Complex (Complex((:+)), realPart, imagPart, magnitude)
 
 
 -- Signature works like: Param1 -> Param2 -> Return type
-escapesToInf :: Float -> Float -> Int -> Bool
+escapesToInf :: Complex Double -> Complex Double -> Int -> Complex Double
 
 -- TODO: calculate equation manually and check if function is being calculated properly (Seems to return 3 in some cases where it LIKELY shouldn't but unsure)
 -- Based on: https://www.futurelearn.com/info/courses/functional-programming-haskell/0/steps/27226 (Reference)
 -- ! TODO: Can this singular function be used for both Julia & Mandlebrot sets? (Figure out)
 -- n = z_0 | c = constant | d = iteration depth
 escapesToInf n c d -- C1: Recursion
-    | d < 0 = False -- Base Case
-    | n > 2 = True -- If escapes into infinity, return 3 (TODO: Create better handling for this)
+    | d < 0 = n -- Base Case
+    | magnitude n > 2 = n -- If escapes into infinity, return 3 (TODO: Create better handling for this)
     | otherwise = escapesToInf (n*n + c) c (d-1) -- Recursive iteration to check whether or not n(z_n) escapes into infinity
 
 -- Recommended depth is around d=500 to check for infinity-escape (.. Source? Read it somewhere... TODO: Cite source)
@@ -84,6 +84,11 @@ px py = selected pixel to map c to.
  -}
 
 
+-- Created separate func, check if z > 2 in separate section
+genZ :: Complex Double -> Complex Double -> Int -> Complex Double
+genZ n c d -- C1: Recursion
+    | d < 0 = n -- Base Case
+    | otherwise = escapesToInf (n*n + c) c (d-1) -- Recursive iteration to check whether or not n(z_n) escapes into infinity
 
 
 genC :: Int -> Int -> Double -> Double -> Double -> Double -> Int -> Int -> Complex Double
@@ -94,11 +99,18 @@ genC w h x_min x_max y_min y_max px py =
 -- Resolve issues when inputting the parameters (Negative numbers can be seen as numeric litearls whilst it is expecting a double)
 -- TODO: Improve type signature & calculations (Somewhere later)
 
+-- Temp, convert later to something that returns 1 or 0 when escaped, or a scale value for it (e.g., 1 - 100 to measure how far it has escaped -> Maybe down the line as diff func)
+testfunc z
+    | magnitude z > 2 = "Escaped: " ++ show (magnitude z)
+    | otherwise = "Within bounds: " ++ show (magnitude z)
 
 main :: IO ()
 main = do
-    let x = genC 200 200 (-2) 1 (-1.5) 1.5 200 200
-    putStrLn $ "Real part: " ++ show (realPart x)
-    putStrLn $ "Imag part: " ++ show (imagPart x)
+    let x = genC 200 200 (-2) 1 (-1.5) 1.5 100 200
+    putStrLn $ "(c) Real part: " ++ show (realPart x)
+    putStrLn $ "(c) Imag part: " ++ show (imagPart x)
+    putStrLn $ "(c) Magnitude: " ++ show (magnitude x)
     -- https://hackage.haskell.org/package/base-4.21.0.0/docs/Text-Show.html
     -- Can convert a function to return it as a String to begin with. Maybe for rendering down the line? Ehh for now Text.Show is fine.
+    let y = genZ 0 x 500
+    putStrLn $ testfunc y
